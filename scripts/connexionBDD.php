b2fc9d23bda1203c7cc877b274df4e4e025783e4
Leo MAKONGUE <Leo.MAKONGUE@sio.lan>
2024-10-09 10:41:36 +0200

    ajout de la connexion de la base BDD

---
+++ scripts/connexionBDD.php
@@ -0,0 +1,28 @@
+<?php
+
+function connexionBDD()
+{
+$bdd = 'mysql:dbname=mariteam;host=localhost';
+$user ='mariteam';
+$password = 'mariteam';
+
+try {
+   
+    $ObjConnexion=new PDO($bdd,$user,$password) ; 
+           //echo 'vous êtes connecté à la base de données' ; 
+           
+}
+ catch (PDOException $e)
+ {
+     echo $e->getMessage();
+ }
+return $ObjConnexion;
+}
+
+function deconnexionBDD($cnx)
+{
+    $cnx=null;
+}
+
+
+?>
