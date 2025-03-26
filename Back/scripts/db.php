93e992ac73f60d9ed86787d979e35f492886ec56
NoreddineBenkhalifa <noreddine.bklf@gmail.com>
2025-03-26 10:31:08 +0100

    RESOLUTION

--- Back/scripts/db.php
+++ Back/scripts/db.php
@@ -10,7 +10,6 @@ function connexionBDD() {
     try {
         $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "vous êtes connecté à la base de données<br>";
         return $pdo; // Assurez-vous de retourner $pdo
     } catch (PDOException $e) {
         echo "Erreur de connexion à la base de données : " . $e->getMessage();
